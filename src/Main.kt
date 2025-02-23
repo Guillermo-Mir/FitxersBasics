43a8d8d46e4ee3621b16d29a756ef76ef457eaee
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-02-23 12:26:04 +0100

    Primer sense terminar

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
+import java.io.File
+import java.util.Scanner
+import kotlin.io.path.*
+
 fun main() {
-    println("Hello World!")
+
+   val ruta = Path("IdeaProjects/FitxersBasics/src/textos.txt")
+
+   do {
+      println("Escriu per teclat per guardar les paraules al fitxer ")
+      var frase = readln()
+      if (frase != " "){
+         ruta.writeText("${frase}\n")
+      }
+   }while (frase != "")
 }
